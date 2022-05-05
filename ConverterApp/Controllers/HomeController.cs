211eff4e15cffb334de10492d07666719843b028
mustafacolakoglu28 <mstfclkglu@gmail.com>
2022-05-05 13:11:20 +0200

    Initial check-in of module ConverterApp

---
+++ ConverterApp/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Syncfusion.Drawing;
+
+using Microsoft.AspNetCore.Mvc;
+using ConverterApp.Models;
+using System.IO;
+using Syncfusion.Pdf.Graphics;
+using Syncfusion.Pdf;
+
+using Microsoft.AspNetCore.Http;
+
+namespace ConverterApp.Controllers
+{
+    public class HomeController : Controller
+
+    {
+       
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult ConvertToPDF(IList<IFormFile> files)
+        {
+            //Creating the new PDF document
+            PdfDocument document = new PdfDocument();
+
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    MemoryStream file = new MemoryStream();
+                    formFile.CopyTo(file);
+                    //Loading the image
+                    PdfImage image = PdfImage.FromStream(file);
+                    //Adding new page
+                    PdfPage page = page = document.Pages.Add();
+
+                    //Drawing image to the PDF page
+                    page.Graphics.DrawImage(image, new PointF(0, 0));
+                    file.Dispose();
+                }
+            }
+            //Saving the PDF to the MemoryStream
+            MemoryStream stream = new MemoryStream();
+
+            document.Save(stream);
+
+            //Set the position as '0'.
+            stream.Position = 0;
+
+            //Download the PDF document in the browser
+            FileStreamResult fileStreamResult = new FileStreamResult(stream, "application/pdf");
+
+            fileStreamResult.FileDownloadName = "ImageToPDF.pdf";
+
+            return fileStreamResult;
+        }
+
+
+
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
